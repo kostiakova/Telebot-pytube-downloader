1e4f1ab963cd6d7f9606e1a6133f26b7c1eab138
kostiakova <94868851+kostiakova@users.noreply.github.com>
2022-10-17 01:43:31 +0300

    Add files via upload

--- telebot-pytube-downloader.py
+++ telebot-pytube-downloader.py
@@ -1,48 +1,77 @@
 import random
 import pytube
-from aiogram import  Dispatcher, Bot, types
+from aiogram import Dispatcher, Bot, types
 from aiogram.utils import executor
 from glob import glob
+import os
 
-token = "5084563840:AAEPCiX5oa8JScbexo2eUidZ00Y95Z0DuDE"
-bot = Bot(token)
+current_path = os.getcwd()
+token = open(current_path+"\\res\\api.txt", "r").readline()
+bot = Bot(str(token))
 dp = Dispatcher(bot)
 sending = ""
 
-async def onStart(_):
+async def onStartInit(_):
     print("Bot is on-line!")
 
-list_mp3 = glob('/home/kostinus/Videos/*')
+list_mp3 = glob(current_path + "current_path + '\\*")
 
 @dp.message_handler(commands=["start"])
 async def onStart(message:types.Message):
-    await bot.send_message(message.from_user.id, "This is bot, developed by KostiaKova.\nThe easiest form of this class bots.\nSource code: ")
+    await bot.send_message(message.from_user.id, "This is bot, developed by KostiaKova.\nThe easiest form of this class bots.\nSource code:\n"
+                                                 "https://github.com/kostiakova/telebot-pytube-downloader.git")
 
 @dp.message_handler(commands=['random'])
-async def command_start(message:types.Message):
+async def Random(message:types.Message):
     await  bot.send_audio(message.from_user.id, audio=open(random.choice(list_mp3), "rb"))
 
 @dp.message_handler(commands=["send"])
-async def Sending(message:types.Message):
-    with open("res.txt", "r") as fr:
+async def Sending_downloaded(message:types.Message):
+    with open(current_path + "\\res\\last_downloading.txt", "r") as fr:
         string_name = fr.read(100)
         fr.close()
-    await bot.send_audio(message.from_user.id, audio=open("/home/kostinus/Videos/"+string_name+".mp3", "rb"))
+    await bot.send_audio(message.from_user.id, audio=open(current_path+"\\res\\audios\\"+string_name+".mp3", "rb"))
 
 @dp.message_handler(lambda message: True)
-async def checkingMsgs(message:types.Message):
-    link = message.text
-    yt = pytube.YouTube(link)
-    name = yt.title
-    stream = yt.streams.filter(only_audio=True).first()
-    stream.download(output_path="/home/kostinus/Videos",
-                    filename=name+".mp3")
-    print(name)
-    sending = name
-    print(sending)
-    with open("res.txt", "w") as fw:
-        fw.write(sending)
-        fw.close()
+async def checkingAllMsgs(message:types.Message):
+    print(message.text)
+    if "www" in message.text:
+        link: str = message.text
+        yt = pytube.YouTube(link)
+        name: str = yt.title
+        stream = yt.streams.filter(only_audio=True).first()
+        stream.download(output_path=current_path+"\\res\\audios\\", filename=name+".mp3")
+        print(name)
+        with open(current_path + "\\res\\last_downloading.txt", "w") as fw:
+            fw.write(name)
+            fw.close()
+        print("Downloading is DONE!!!")
+        await bot.send_message(message.from_user.id, "Downloading is Well DONE!")
+        print("Proceeding to sending")
+        await bot.send_message(message.from_user.id, "Proceeding to Sending")
+        try:
+            await Sending_downloaded(message)
+        except:
+            print("Didn't Send :(")
+            await bot.send_message(message.from_user.id, "I didn't Send it:(")
 
+    else:
+        await bot.send_message(message.from_user.id, "This message isn't link")
+    # link = message.text
+    # yt = pytube.YouTube(link)
+    # name = yt.title
+    # stream = yt.streams.filter(only_audio=True).first()
+    # stream.download(output_path="/home/kostinus/Videos",
+    #                 filename=name+".mp3")
+    # print(name)
+    # sending = name
+    # print(sending)
+    # with open("res.txt", "w") as fw:
+    #     fw.write(sending)
+    #     fw.close()
 
-executor.start_polling(dp, skip_updates=True, on_startup=onStart)
+@dp.message_handler(commands=["stop"])
+def Stop():
+    exit()
+
+executor.start_polling(dp, skip_updates=True, on_startup=onStartInit)
